b617900d92fce21d3de443198ec2b3953d891190
darco2018 <newsecond@outlook.com>
2019-05-19 13:33:05 +0200

    Add colors to squares

--- src/js/index.js
+++ src/js/index.js
@@ -13,18 +13,49 @@ if (jQuery) {
 
 $(document).ready(function main() {
   const colorGame = (() => {
+    const rgbLowerBound = 0;
+    const rgbUpperBound = 256;
     const squares = document.querySelectorAll(".square");
+    const displayElem = ".rgb-text";
+    let currentColor = "rgb(0, 0, 0)";
 
-    const addListeners = () => {
-      squares.forEach(elem => {
+    const getRandomInt = (minInclusive, maxExclusive) => {
+      const minInt = Math.ceil(minInclusive);
+      const maxInt = Math.floor(maxExclusive);
+      return Math.floor(Math.random() * (maxInt - minInt)) + minInt;
+    };
+
+    const getRandomColor = () => {
+      const red = getRandomInt(rgbLowerBound, rgbUpperBound);
+      const green = getRandomInt(rgbLowerBound, rgbUpperBound);
+      const blue = getRandomInt(rgbLowerBound, rgbUpperBound);
+      const color = `rgb(${red}, ${green}, ${blue})`;
+      return color;
+    };
+
+    const showOnPage = (color, selector) => {
+      document.querySelector(selector).textContent = color;
+    };
+
+    const addListenersTo = arr => {
+      arr.forEach(elem => {
         elem.addEventListener("click", () => {
           console.log("hi");
         });
       });
     };
+    const addColorTo = arr => {
+      arr.forEach(e => {
+        e.style.backgroundColor = getRandomColor();
+      });
+    };
 
     const init = () => {
-      addListeners();
+      addListenersTo(squares);
+      addColorTo(squares);
+      currentColor = getRandomColor();
+      showOnPage(currentColor, displayElem);
+      console.log();
     };
 
     return { init };
