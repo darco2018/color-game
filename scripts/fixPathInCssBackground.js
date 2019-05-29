6e419e879f97797845205e96efa2754f928236d9
darco2018 <newsecond@outlook.com>
2019-05-29 15:20:11 +0200

    Add custom script, description to package.json

---
+++ scripts/fixPathInCssBackground.js
@@ -0,0 +1,5 @@
+/** @format */
+const utils = require("./utils");
+// dist/assets/css/index.css
+// //home/ustrd/Documents/javascript-projects/portfolio/src/project/response-tester/dist/assets/css/index.css
+utils.replaceInFile("dist/assets/css/index.css", "assets", "..");
