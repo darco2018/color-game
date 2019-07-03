b5e4a8c5ba816bea313af808ca665229b993eb83
darco2018 <newsecond@outlook.com>
2019-07-03 15:59:59 +0200

    Modify prettier & webpack

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@
 // dont pass options to Prettier via your ESLint configuration file - VS can't read it then
 module.exports = {
   insertPragma: false, // not working with eslint-loader
-  quoteProps: "consistent",
+  quoteProps: "consistent"
 
   // tabWidth: 4  >> change for testing purposes
 };
